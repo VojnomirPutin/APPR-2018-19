8e189af8eea59008c872dbe5dfec6663fe61e05e
DomenFlakusBosilj <domen.4fb@gmail.com>
2019-01-05 23:49:54 +0100

    zacetek vizualizacije

--- vizualizacija/vizualizacija_domen.R
+++ vizualizacija/vizualizacija_domen.R
@@ -4,11 +4,14 @@ library(dplyr)
 library(rgdal)
 library(mosaic)
 library(maptools)
+library(ggmap)
+library(mapproj)
 
 graf_bdppc <- ggplot(data = bdppc, mapping = aes(x=Leto, y=Kolicina_eur, fill=Drzava))
 graf_bdppc <- graf_bdppc + geom_bar(stat = 'identity', position = 'dodge')
 #to se malo polepsaj
 
 #plot(graf_bdppc)
+
 
 
