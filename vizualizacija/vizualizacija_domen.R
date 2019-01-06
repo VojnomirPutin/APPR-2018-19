31117b85832c0840ff4c2d07ba7a7f8336add761
Žan Kramar <42943735+VojnomirPutin@users.noreply.github.com>
2019-01-06 13:57:10 +0100

    probaj to zalaufat

--- vizualizacija/vizualizacija_domen.R
+++ vizualizacija/vizualizacija_domen.R
@@ -7,8 +7,8 @@ library(maptools)
 library(ggmap)
 library(mapproj)
 
-graf_bdppc <- ggplot(data = bdppc, mapping = aes(x=Leto, y=Kolicina_eur, fill=Drzava))
-graf_bdppc <- graf_bdppc + geom_bar(stat = 'identity', position = 'dodge')
+graf_bdppc <- ggplot(data = bdppc, aes(x=Leto, y=Kolicina_eur, col=Drzava)) + geom_point() + geomline()
+#graf_bdppc <- graf_bdppc + geom_bar(stat = 'identity', position = 'dodge')
 #to se malo polepsaj
 
 #plot(graf_bdppc)
